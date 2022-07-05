bf4834c2b4689598b7bb5a9d49cc65998344d836
minh khue <dominh3214@gmail.com>
2022-07-05 17:21:15 +0700

    search by company,name,feature

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,32 @@
+const Product = require('../models/product')
 const getAllProductsStatic = async (req,res) =>{
-    throw new Error('Testing aync errors')
-    res.status(200).json({msg:'product testing route'})
+    const search = 'ab'
+    const products = await Product.find({
+        name:{$regex:search,$options:'1'}
+    })
+    res.status(200).json({products,nbHits:products.length})
+    // if(!products){
+    //     throw new Error('Cant get data')
+    // }
 }
 
 const getAllProducts = async (req,res) =>{
-    res.status(200).json({msg:'product route'})
+    const {featured,company,name} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === "true" ? true : false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = name
+    }
+    // if featured is false ,we gat all the data
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products,nbHits:products.length})
 }
 
 module.exports = {
